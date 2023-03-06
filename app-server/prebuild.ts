341174419d70417ab6a90086a231224070b934c4
Josep Llodrà Grimalt <>
2023-03-06 16:33:52 +0100

    add server tsconfig

---
+++ app-server/prebuild.ts
@@ -0,0 +1,14 @@
+/*
+ * @license
+ * Copyright Atlantis of Code All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/atlantis-of-code/aoc/blob/main/LICENSE
+ *
+ */
+
+const config = require('./tsconfig.json'); // /dist
+const outDir = config.compilerOptions.outDir;
+const srcDir = 'src';
+
+// Do here all the operations you need to be done previous to building...
