391ec3d000c1fe388842e6ecd54b005e0d0a6b33
Josep Llodrà Grimalt <>
2023-03-07 17:39:45 +0100

    fill app-routing.module.ts

--- app-client/src/app/app-routing.module.ts
+++ app-client/src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AocLoginComponent } from '@atlantis-of-code/aoc-client/components/aoc-login';
+import { AocTabConfig } from '@atlantis-of-code/aoc-client/core/configs';
+import { AocAuthGuard, AocLoginGuard } from '@atlantis-of-code/aoc-client/core/guards';
+import { of } from 'rxjs';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: AocLoginComponent,
+    canActivate: [AocLoginGuard],
+    data: {
+      title: of('Login')
+    } as AocTabConfig
+  },
+  {
+    path: '',
+    canActivate: [AocAuthGuard],
+    children: [
+      /*{ // DASHBOARD
+        path: 'dashboard',
+        component: DashboardComponent,
+        data: {
+          title: of('Dashboard'),
+          closable: false
+        } as AocTabConfig
+      }*/
+      {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+      {path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: false })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
