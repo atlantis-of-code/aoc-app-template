b5687ec3e215a1bba2731fbd192ee5a91c5ecb55
Josep Llodrà Grimalt <>
2023-03-07 15:39:48 +0100

    add app-common

---
+++ app-common/src/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @license
+ * Copyright Atlantis of Code All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://github.com/atlantis-of-code/aoc/blob/main/LICENSE
+ *
+ */
+
+export { ExampleCommonInterface } from './example-common';
