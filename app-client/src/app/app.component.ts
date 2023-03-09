aede5d12f8e7a62050f910114868a149b1989b30
Josep Llodrà Grimalt <>
2023-03-09 13:47:32 +0100

    add package.json to root folder and concurrently lib

--- app-client/src/app/app.component.ts
+++ app-client/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AocModelManager } from '@atlantis-of-code/aoc-client/core/models';
+import { AocUiLoggerService } from '@atlantis-of-code/aoc-client/ui/common/services';
+import { AocUiDataMenu } from '@atlantis-of-code/aoc-client/ui/common/types';
+import { meta } from './models/meta';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-client';
+  mainMenu: AocUiDataMenu = [
+    {
+      label: 'Settings',
+      icon: 'settings',
+      items: [
+        { label: 'Users', routerLink: ['users', 'user', 'panel' ] }
+      ]
+    }
+  ];
+
+  headerMenu: AocUiDataMenu = [];
+
+  constructor(private logger: AocUiLoggerService) {
+    this.logger.info('Starting app-client app.component.ts');
+    AocModelManager.registerMeta(meta);
+  }
 }
--- app-client/src/app/app.module.ts
+++ app-client/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AocUserConfig } from '@atlantis-of-code/aoc-client/aoc-common';
 import { AocAppModule } from '@atlantis-of-code/aoc-client/components/aoc-app';
 import { AocDirectivesModule } from '@atlantis-of-code/aoc-client/core/directives';
+import { Config } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { User } from './models/users/user';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,22 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     AppRoutingModule,
     AocDirectivesModule,
-    AocAppModule//.forRoot({}, {})
+    AocAppModule.forRoot(
+      Config,
+      new AocUserConfig(User, { fieldMap: { username: 'username', password: 'pass' }}),
+      {
+        autoConvertEmptyStringsToNullInControls: true,
+        autoTrimStringsInControls: true,
+        dateTimePicker: {
+          mode: 'dateTime',
+          formats: {
+            date: 'dd/MM/y',
+            dateTime: 'dd/MM/y HH:mm:ss',
+            time: 'HH:mm:ss'
+          }
+        }
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
