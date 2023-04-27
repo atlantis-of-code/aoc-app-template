69d89377e3aacec7ffe52fc7a80b2b74605401f6
Josep Llodrà <273590+jllodra@users.noreply.github.com>
2023-04-27 18:22:19 +0200

    remove license notice

--- app-client/src/app/models/app-template-model.ts
+++ app-client/src/app/models/app-template-model.ts
@@ -1,12 +1,3 @@
-/*
- * @license
- * Copyright Atlantis of Code All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://github.com/atlantis-of-code/aoc/blob/main/LICENSE
- *
- */
-
 import { AocModel } from '@atlantis-of-code/aoc-client/core/models';
 
 export class AppTemplateModel extends AocModel<string> {
